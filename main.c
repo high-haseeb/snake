9d97cbe0821f5adf92800396808006dc0d7efd40
high-haseeb <haseebkhalidoriginal@gmail.com>
2023-08-25 05:59:05 +0500

    made some changes

--- main.c
+++ main.c
@@ -1,71 +1,129 @@
+#include <curses.h>
+#include <math.h>
 #include <ncurses.h>
-#include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
-#define ROWS 10
-#define COLS 10
+#define ROWS 20
+#define COLS 40
 
-int head_x = 0;
-int head_y = 0;
+bool gameover = false;
+float speed = 0.001;
 int score = 0;
-int gameover = 0;
 
-void draw(void);
-void setup(void);
-void gameloop(void);
-void drawSnake(void);
-void update(void);
-
-int main(void) {
-  // setup();
-  gameloop();
-  return 0;
+struct Snake {
+  float head_x;
+  float head_y;
+  char dir;
+  int body[ROWS + COLS][2];
+  int length;
+};
+struct Food {
+  int x;
+  int y;
+};
+struct Snake snake;
+struct Food food;
+void init(void) {
+  snake.head_y = 5;
+  snake.head_x = 10;
+  snake.length = 0;
+  food.x = rand() % COLS;
+  food.y = rand() % ROWS;
 }
+void draw(void) {
+  move(0, 0);
+  refresh();
+  printw("  score:>>> %i\n", score);
+  for (int i = 0; i < COLS + 2; i++)
+    printw("_");
 
-void gameloop(void) {
-  while (!gameover) {
-    draw();
-    update();
+  printw("\n");
+  for (int y = 0; y < ROWS; y++) {
+    addstr("|");
+    for (int x = 0; x < COLS; x++) {
+      if ((int)snake.head_y == y && (int)snake.head_x == x) {
+        addstr("#");
+      } else if (food.x == x && food.y == y) {
+        addstr("O");
+      } else {
+        for (int l = 0; l < snake.length; l++) {
+          if (snake.body[l][0] == x && snake.body[l][1] == y)
+            addstr("x");
+        }
+      }
+      addstr(" ");
+    }
+    addstr("|\n");
   }
+
+  for (int i = 0; i < COLS + 2; i++)
+    printw("_");
 }
 void update(void) {
-  switch (getch()) {
-    case KEY_UP:
-      head_y += 1;
-      break;
-    case KEY_DOWN:
-      head_y -= 1;
-      break;
-    case KEY_LEFT:
-      head_x += 1;
-      break;
-    case KEY_RIGHT:
-      head_x -= 1;
-      break;
+  switch (snake.dir) {
+  case 'N':
+    snake.head_y -= speed;
+    break;
+  case 'S':
+    snake.head_y += speed;
+    break;
+  case 'E':
+    snake.head_x += speed;
+    break;
+  case 'W':
+    snake.head_x -= speed;
+    break;
   }
-  return;
+  for (int i = score; i > 0; i--) {
+    snake.body[i][0] = snake.body[i - 1][0];
+    snake.body[i][1] = snake.body[i - 1][1];
+  }
+  snake.body[0][0] = (int)snake.head_x;
+  snake.body[0][1] = (int)snake.head_y;
 }
-void setup(void) { draw(); }
-void draw(void) {
-  system("clear");
-  for (int i = 0; i < COLS; i++)
-    printf("_");
-
-  printf("\n");
-  for (int i = 0; i < ROWS; i++) {
-    printf("|");
-    for (int j = 0; j < COLS; j++) {
-      if (i == head_y && j == head_x) {
-        printf("#");
-      } else {
-        printf(" ");
-      }
-    }
-    printf("|\n");
+void input(void) {
+  int chr = getch();
+  switch (chr) {
+  case KEY_UP:
+    snake.dir = 'N';
+    break;
+  case KEY_DOWN:
+    snake.dir = 'S';
+    break;
+  case KEY_LEFT:
+    snake.dir = 'W';
+    break;
+  case KEY_RIGHT:
+    snake.dir = 'E';
+    break;
   }
-
-  for (int i = 0; i < COLS; i++)
-    printf("_");
-
-  printf("\n");
+}
+void spawnFood(void) {
+  food.x = rand() % COLS;
+  food.y = rand() % ROWS;
+}
+void logic(void) {
+  if (snake.head_x <= 0 || snake.head_x >= COLS || snake.head_y <= 0 ||
+      snake.head_y >= ROWS) {
+    gameover = true;
+  }
+  if ((int)snake.head_x == food.x && (int)snake.head_y == food.y) {
+    score++;
+    spawnFood();
+  }
+}
+int main() {
+  initscr(); // init ncurses window
+  init();
+  nodelay(stdscr, true);
+  keypad(stdscr, true);
+  while (!gameover) {
+    input();
+    update();
+    draw();
+    logic();
+  }
+  printw("you lost motherfucker!! you pussy ass noob");
+  return 0;
 }
